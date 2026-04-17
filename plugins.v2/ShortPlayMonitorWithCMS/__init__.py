09237c83b228264c3be4bec47512aa05220b46b5
feiye2021 <feiye2021@users.noreply.github.com>
2026-04-17 16:09:42 +0800

    修正BUG

--- plugins.v2/ShortPlayMonitorWithCMS/__init__.py
+++ plugins.v2/ShortPlayMonitorWithCMS/__init__.py
@@ -70,7 +70,7 @@ class ShortPlayMonitorWithCMS(_PluginBase):
     # 插件版本
     plugin_version = "1.0.0"
     # 插件作者
-    plugin_author = "thsrite,Seed680,imaliang,feiye"
+    plugin_author = "feiye"
     # 作者主页
     author_url = "https://github.com/feiye2021/MoviePilot-Plugins"
     # 插件配置项ID前缀
